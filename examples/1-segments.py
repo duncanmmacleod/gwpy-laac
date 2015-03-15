2349fe05343e89ce0c9d0ab56262ee08ed0c99eb
Duncan Macleod <duncan.macleod@ligo.org>
2015-03-15 13:28:24 -0500

    example/1: fix example for ligolw_segment_query_dqsegdb

--- examples/1-segments.py
+++ examples/1-segments.py
@@ -11,7 +11,7 @@ Data Quality Segment DataBase (DQSegDB), and plot them.
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
 __currentmodule__ = 'gwpy.segments'
 
-# First, import the `DataQualityFlag` object
+# First, import the `~gwpy.segments.DataQualityFlag` object
 from gwpy.segments import DataQualityFlag
 
 # Then call the `DataQualityFlag.query_dqsegdb()
@@ -23,7 +23,19 @@ intent = DataQualityFlag.query_dqsegdb(
 
 # And that's it.
 #
-# Now, we can plot them for sanity by calling the `~DataQualityFlag.plot`
-# method:
+# Alternatively, you could have done the same from the command-line, using
+# the `ligolw_segment_query_dqsegdb <https://ldas-jobs.ligo.caltech.edu/~rfisher/dqsegdb_doc/ligolw_segment_query_dqsegdb.html>`_ script provided by the
+# `DQSegDB <https://www.lsc-group.phys.uwm.edu/daswg/projects/dqsegdb.html>`_
+# package:
+#
+# .. code-block:: bash
+#
+#    ligolw_segment_query_dqsegdb --query-segments --segment-url https://dqsegdb5.phy.syr.edu -s 1109203216 -e 1109808016 --include-segments L1:DMT-SCIENCE:1
+#
+# Which would return a `LIGO_LW` XML document complete with a set of segment
+# tables (`segment_definer`, `segment_summary`, and `segment`).
+#
+# Finally, we can plot the segments downloaded in GWpy by calling the
+# :meth:`~gwpy.segments.DataQualityFlag.plot` method:
 plot = intent.plot(label='LLO')
 plot.show()
