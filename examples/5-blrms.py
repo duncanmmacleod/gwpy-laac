c0f26d1d0cfc46a86133ce086b8e68e868b63dff
Duncan Macleod <duncan.macleod@ligo.org>
2015-03-15 10:00:38 -0700

    examples/5: remove license, fix intersphinx

--- examples/5-blrms.py
+++ examples/5-blrms.py
@@ -1,21 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Copyright (C) Duncan Macleod (2013-2015)
-#
-# This file is part of GWpy.
-#
-# GWpy is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# GWpy is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with GWpy.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (C) Duncan Macleod (2015)
 
 """How to read band-limited RMS seismic trends and plot them
 
@@ -42,7 +27,7 @@ channels = [
 ]
 
 # At last we can fetch 12 hours of data for each interferometer using the
-# `TimeSeriesDict.fetch` method:
+# `TimeSeriesDict.fetch() <gwpy.timeseries.TimeSeries.fetch>` method:
 lho = TimeSeriesDict.fetch([c % 'H1' for c in channels],
                            'Feb 13 2015 16:00', 'Feb 14 2015 04:00')
 llo = TimeSeriesDict.fetch([c % 'L1' for c in channels],
